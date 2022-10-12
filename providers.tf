4c8b5ad009a878fb8978391eb4645f423685cd61
sanjay123123 <64998669+sanjay123123@users.noreply.github.com>
2022-10-12 17:32:45 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
 
   required_version = ">=0.12"
-  
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>3.9.0"
     }
   }
-  
+
 }
 
 provider "azurerm" {
