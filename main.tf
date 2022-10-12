72bb5d6cf347f9fff7e08f5a1cbc17de5b7f7b5b
sanjay123123 <64998669+sanjay123123@users.noreply.github.com>
2022-10-12 17:36:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+}
+
+
 data "azuread_client_config" "current" {}
 
 resource "azuread_group" "TestADGroup" {
