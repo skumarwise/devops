477ca29f460e3c86f6e22673da306f1f8728b9ff
sanjay123123 <64998669+sanjay123123@users.noreply.github.com>
2022-10-12 17:35:36 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,17 +1,23 @@
+# --------------------------------------------------------------------------------------------------
+# Terraform
+# --------------------------------------------------------------------------------------------------
+
 terraform {
-
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.9.0"
-    }
-  }
-
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = "true"
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
 }
